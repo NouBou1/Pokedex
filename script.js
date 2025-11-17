9b7b0ce9e4c15efc50bdb95c8219353d65603961
NouBou1 <n.boussaada92@gmail.com>
2025-11-17 22:21:29 +0100

    renderDialogCard fixed - shows up now - index still not correct

--- script.js
+++ script.js
@@ -5,58 +5,18 @@ const color_URL = "https://pokeapi.co/api/v2/pokemon-color/";
 let pokeList;
 let pokeImg;
 let dataList = [];
-let imgList = [];
+let pokemonType = dataList[indexPokemon].types.;
 
 
 
 async function init() {
   pokeList = await getPokemon();
-
   renderPokeCard(pokeList);
-  // renderPokeCardDialog(pokeList);
   console.log(pokeList);
+  getPokeTypes(indexPokemon);
 }
 
-//  load mainpage
-// async function getPokemon() {
-//   let pokedex_URL = base_URL + "pokemon?limit=20&offset=0";
-//   let response = await fetch(pokedex_URL);
-//   let responseToJson = await response.json();
-//   console.log(responseToJson);
-//   return responseToJson.results;
-// }
-
-// async function getPokemon() {
-//   const pokedex_URL = base_URL + "pokemon?limit=20&offset=0";
-//   const response = await fetch(pokedex_URL);
-//   const data = await response.json();
-
-//   for (let p of data.results) {
-//     const pokemonRes = await fetch(p.url);
-//     const pokemonData = await pokemonRes.json();
-//     dataList.push(pokemonData);
-//   }
-
-//   return dataList;
-// }
-
-
-// async function getPokemonImg() {
-//   const img_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/";
-//   const responseImg = await fetch(img_URL + (indexPokemon +1) + ".png");
-//   const img = await responseImg.json();
-
-//   for (const pic of img.results) {
-//     const pokemonImgRes = await fetch(pic.url);
-//     const pokemonImgData = await pokemonImgRes.json();
-//     pokemonImgData.push(imgList);
-//   }
-//   // console.log(imgList)
-//   return imgList;
-// }
-
-
-async function getPokemon(indexPokemon) {
+async function getPokemon() {
   const pokedex_URL = base_URL + "pokemon?limit=20&offset=0";
   const response = await fetch(pokedex_URL);
   const data = await response.json();
@@ -72,28 +32,22 @@ async function getPokemon(indexPokemon) {
 }
 
 
-function renderPokeCard(pokemonImg, indexPokemon) {
+function renderPokeCard(pokemonImg) {
   let pokeCardRef = document.getElementById('card_section_content');
   for (let indexPokemon = 0; indexPokemon < dataList.length; indexPokemon++) {
-    let pokemonImg = dataList[indexPokemon].sprites.other.home.front_default
+    let pokemonImg = dataList[indexPokemon].sprites.other.home.front_default;
     pokeCardRef.innerHTML += getPokeCardTemplate(indexPokemon, pokemonImg);
   }
   console.log(pokemonImg)
 }
 
-// img_URL + (indexPokemon + 1) + ".png";
-
-// dialog
-function renderPokeCardDialog(pokemonImg) {
-  let pokeCardDialogRef = document.getElementById('dialog_content');
+function renderPokeCardDialog(indexPokemon) {
+  const pokeCardDialogRef = document.getElementById('dialog_content');
   pokeCardDialogRef.innerHTML = "";
-  let dialogImg = dataList[indexPokemon].sprites.other.home.front_default
-  for (let indexPokemonDialog = 0; indexPokemonDialog < dataList.length; indexPokemonDialog++) {
-    pokeCardDialogRef.innerHTML += getPokeCardDialogTemplate(indexPokemonDialog, dialogImg);
-  }
+  const pokemon = dataList[indexPokemon];
+  const dialogImg = pokemon.sprites.other.home.front_default;
+  pokeCardDialogRef.innerHTML = getPokeCardDialogTemplate(indexPokemon, dialogImg);
 }
-
-
 
 function showCardDialog(indexPokemon) {
   let cardDialogRef = document.getElementById('dialog_pokecard');
@@ -101,11 +55,6 @@ function showCardDialog(indexPokemon) {
   renderPokeCardDialog(indexPokemon);
 }
 
-console.log({
 
-  pokeListIsArray: Array.isArray(dataList),
-  pokeListLen: dataList?.length,
- 
 
-});
-
+console.log(indexPokemon)
--- templates/template.js
+++ templates/template.js
@@ -1,32 +1,33 @@
-function getPokeCardTemplate(indexPokemon, pokemonImg, indexPokemonDialog) {
-    return `<div class="pokecard_style" id="pokecard" onclick="showCardDialog('${indexPokemon}')";  tabindex="0" >
+function getPokeCardTemplate(indexPokemon, pokemonImg, pokemonType) {
+    return `
+    <div class="pokecard_style" id="pokecard" onclick="showCardDialog('${indexPokemon}')";  tabindex="0" >
                     <div id="card_header" class="card_header">
                         <div id="card_number" class="card_number">${indexPokemon + 1}</div>
                         <div id="card_name" class="card_name">${pokeList[indexPokemon].name}</div>
                     </div>
                         <img class="card_img" src="${pokemonImg}" alt="">
-                    <div id="card_footer" class="card_footer">stats</div>
+                    <div id="pokemon_type" class="card_footer">${pokemonType}</div>
+                    <div> </div>
                 </div>
             </div>
 `}
 
-function getPokeCardDialogTemplate(indexPokemon, pokemonImg){
-    return `<div class="pokecard_style"   >
-                    <div class="card_header">
-                        <div class="card_number">${indexPokemon}</div>
-                        <div class="card_name">${pokeList[indexPokemon].name}</div>
-                    </div>
-                    <div>
-                        <img class="card_img" src="${img_URL + (indexPokemon + 1) + ".png"}" alt="">
-                    </div>
-                    <div> 
-                    <div class="card_footer">stats</div>
-                </div>
-                <ul>
-                <li>       </li>
-                <li>       </li>
-                <li>       </li>
-                </ul>
-                </div>
-            `}
 
+function getPokeCardDialogTemplate(indexPokemon, dialogImg) {
+  return `
+    <div>
+      <div class="card_header">
+        <div class="card_number">${indexPokemon + 1}</div>
+        <div class="card_name">${pokeList[indexPokemon].name}</div>
+      </div>
+      <div>
+        <img class="card_img" src="${dialogImg}" alt="${pokeList[indexPokemon].name}">
+      </div>
+      <div class="card_footer">stats</div>
+      <ul>
+        <li>       </li>
+        <li>       </li>
+        <li>       </li>
+      </ul>
+    </div>
+  `}
