50082fcffb3a2fefd4e26653cd4f0d4feeb6ed48
NouBou1 <n.boussaada92@gmail.com>
2025-11-23 17:55:02 +0100

    getPokemon -> try&catch  - card bg - card type icons -  getNextPokemon() added

--- script.js
+++ script.js
@@ -2,42 +2,83 @@ const base_URL = "https://pokeapi.co/api/v2/";
 const img_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/";
 const type_URL = "https://pokeapi.co/api/v2/type/1/"
 const color_URL = "https://pokeapi.co/api/v2/pokemon-color/";
+
 let pokeList;
 let pokeImg;
 let dataList = [];
+const limit = 20;
+let offset = 0;
 
-
+let typeColors ={
+    bug: { icon: "./assets/icons/bug.svg", color: "#A6B91A" },
+    dark: { icon: "./assets/icons/dark.svg", color: "#705746" },
+    dragon: { icon: "./assets/icons/dragon.svg", color: "#6F35FC" },
+    electric: { icon: "./assets/icons/electric.svg", color: "#F7D02C" },
+    fairy: { icon: "./assets/icons/fairy.svg", color: "#D685AD" },
+    fighting: { icon: "./assets/icons/fighting.svg", color: "#C22E28" },
+    fire: { icon: "./assets/icons/fire.svg", color: "#EE8130" },
+    flying: { icon: "./assets/icons/flying.svg", color: "#A98FF3" },
+    ghost: { icon: "./assets/icons/ghost.svg", color: "#735797" },
+    grass: { icon: "./assets/icons/grass.svg", color: "#7AC74C" },
+    ground: { icon: "./assets/icons/ground.svg", color: "#E2BF65" },
+    ice: { icon: "./assets/icons/ice.svg", color: "#96D9D6" },
+    normal: { icon: "./assets/icons/normal.svg", color: "#A8A77A" },
+    poison: { icon: "./assets/icons/poison.svg", color: "#A33EA1" },
+    psychic: { icon: "./assets/icons/psychic.svg", color: "#F95587" },
+    rock: { icon: "./assets/icons/rock.svg", color: "#B6A136" },
+    steel: { icon: "./assets/icons/steel.svg", color: "#B7B7CE" },
+    water: { icon: "./assets/icons/water.svg", color: "#6390F0" },
+}
 
 async function init() {
   pokeList = await getPokemon();
-  renderPokeCard(pokeList);
+  renderPokeCard();
   console.log(pokeList);
-  getPokeTypes(indexPokemon);
 }
 
 async function getPokemon() {
-  const pokedex_URL = base_URL + "pokemon?limit=20&offset=0";
-  const response = await fetch(pokedex_URL);
-  const data = await response.json();
+  const pokedex_URL = `${base_URL}pokemon?limit=${limit}&offset=${offset}`;
 
-  for (let p of data.results) {
-    console.log("P =", p)
-    const pokemonRes = await fetch(p.url);
-    const pokemonData = await pokemonRes.json();
-    dataList.push(pokemonData);
-    console.log("DETAIL:", dataList)
+  try {
+    const response = await fetch(pokedex_URL);
+    if (!response.ok) {
+      throw new Error(`API response Not OK: ${response.status}`);
+    }
+    const data = await response.json();
+    for (let p of data.results) {
+      try {
+        const pokemonRes = await fetch(p.url);
+        if (!pokemonRes.ok) {
+          throw new Error(`loading issue  ${p.name}: ${pokemonRes.status}`);
+        }
+        const pokemonData = await pokemonRes.json();
+        dataList.push(pokemonData);
+      } catch (pokemonError) {
+        console.error("problem Pokemon:", pokemonError);
+      }
+    }
+    offset += limit;
+    return dataList;
+
+  } catch (error) {
+    console.error("issue in getPokemon():", error);
+    return [];
   }
-  return dataList;
 }
 
+async function getNextPokemon() {
+    pokeList = await getPokemon();   
+    renderPokeCard();
+  
+}
 
 function renderPokeCard(pokemonImg) {
   let pokeCardRef = document.getElementById('card_section_content');
+  pokeCardRef.innerHTML = "";
   for (let indexPokemon = 0; indexPokemon < dataList.length; indexPokemon++) {
     let pokemonImg = dataList[indexPokemon].sprites.other.home.front_default;
     pokeCardRef.innerHTML += getPokeCardTemplate(indexPokemon, pokemonImg);
   }
-  console.log(pokemonImg)
 }
 
 function renderPokeCardDialog(indexPokemon) {
@@ -54,6 +95,6 @@ function showCardDialog(indexPokemon) {
   renderPokeCardDialog(indexPokemon);
 }
 
-
-
-console.log(indexPokemon)
+function searchPokemon() {
+  
+}
--- templates/template.js
+++ templates/template.js
@@ -1,33 +1,73 @@
-function getPokeCardTemplate(indexPokemon, pokemonImg, pokemonType) {
-    return `
-    <div class="pokecard_style" id="pokecard" onclick="showCardDialog('${indexPokemon}')";  tabindex="0" >
+
+function getPokeCardTemplate(indexPokemon, pokemonImg) {
+  const types = pokeList[indexPokemon].types;
+  const type1 = types[0]?.type?.name;
+  const type2 = types[1]?.type?.name;
+  const icon1 = typeColors[type1]?.icon;
+  const icon2 = typeColors[type2]?.icon
+  const mainType = types[0]?.type?.name;
+  const cardColor = typeColors[mainType]?.color;
+  return `
+    <div class="pokecard_style" id="pokecard"  style="background-color: ${cardColor};" onclick="showCardDialog('${indexPokemon}')";  tabindex="0" >
                     <div id="card_header" class="card_header">
                         <div id="card_number" class="card_number">${indexPokemon + 1}</div>
                         <div id="card_name" class="card_name">${pokeList[indexPokemon].name}</div>
                     </div>
                         <img class="card_img" src="${pokemonImg}" alt="">
-                    <div id="pokemon_type" class="card_footer"</div>
-                    <div> </div>
+                    <div id="pokemon_type" class="card_footer type-icons">
+                    <div>${type1}${type2 ? " , " + type2 : ""}</div>
+                     <div  class="card_footer type-icons">
+        <img class="type-icon" src="${icon1}" alt="${type1}">
+        ${icon2 ? `<img class="type-icon" src="${icon2}" alt="${type2}">` : ""}
+      </div>
+                    
+                    </div>
                 </div>
             </div>
 `}
 
 
 function getPokeCardDialogTemplate(indexPokemon, dialogImg) {
+  const types = pokeList[indexPokemon].types;
+  const mainType = types[0]?.type?.name;
+  const cardColor = typeColors[mainType]?.color;
+  const abilities = pokeList[indexPokemon].abilities;
+  const ability1 = abilities[0]?.ability?.name;
+  const ability2 = abilities[1]?.ability?.name;
   return `
-    <div>
+    <div style="background-color: ${cardColor};">
       <div class="card_header">
-        <div class="card_number">${indexPokemon + 1}</div>
+        <div class="card_number">${pokeList[indexPokemon].id}</div>
         <div class="card_name">${pokeList[indexPokemon].name}</div>
       </div>
       <div>
         <img class="card_img" src="${dialogImg}" alt="${pokeList[indexPokemon].name}">
       </div>
       <div class="card_footer">stats</div>
-      <ul>
-        <li>       </li>
-        <li>       </li>
-        <li>       </li>
-      </ul>
+      <section id="main_details">
+     <table>
+      <tr>
+        <td>Height :</td> <td>  ${pokeList[indexPokemon].height}  dm</td>
+      </tr>
+      <tr>
+        <td>Weight :</td>  <td>  ${pokeList[indexPokemon].weight}  kg</td>
+      </tr>
+      <tr>
+        <td>Experience : </td> <td> ${pokeList[indexPokemon].base_experience}</td>
+      </tr>
+      <tr>
+        <td>Abilities : </td> <td>${ability1}${ability2 ? ", " + ability2 : ""}</td>
+      </tr>
+      </table>
+
+      <div  class=btn_section>
+      <button>zurück</button>
+      <button>vor</button>
+      </div>
+      </section>
+
     </div>
   `}
+
+
+
